9732e3940bc5f416f58dc1050e69bc22fdaa984f
Ewan Breakey <ebreakey@gmail.com>
2023-07-19 16:04:27 +1000

    FIx typo in example for splitArray

--- lib/arrays/splitArray.ts
+++ lib/arrays/splitArray.ts
@@ -6,7 +6,7 @@
  * @returns the array of groups split as per the `splitFn`
  *
  * @example
- * const groups = splitArrayBy([1, 3, 5, 6, 7, 9, 11], (x) => x % 2 === 0) // [[1, 3, 5], [7, 9, 11]]
+ * const groups = splitArray([1, 3, 5, 6, 7, 9, 11], (x) => x % 2 === 0) // [[1, 3, 5], [7, 9, 11]]
  */
 export const splitArray = <T>(arr: T[], splitFn: (t: T) => boolean): T[][] => {
   // Handle special case of empty array
