882ec3ccbe5c460a9d4e5fe4c3717af0e2b6ac40
Benji Grant <benjamin.grantGRA0007@gmail.com>
2023-09-12 20:23:44 +1000

    Add settleObject util

--- lib/promises/resolveObject.ts
+++ lib/promises/resolveObject.ts
@@ -2,27 +2,29 @@ export type Resolved<Type> = { [k in keyof Type]: Awaited<Type[k]> }
 
 /**
  * Resolve all of the fields of an object in parallel
- * @param object An object where every field is a promise 
+ * @param object An object where every field is a promise
  * @returns The same object with every field resolved
- * 
+ *
  * @example await resolveObject({
  *   a: Promise.resolve('a'),
- *   b: Promise.resolve('b'), 
+ *   b: Promise.resolve('b'),
  * }) === { a: 'a', b: 'b' }
  *
  * @example await resolveObject({
  *   a: Promise.resolve('a'),
- *   b: Promise.reject('b'), 
+ *   b: Promise.reject('b'),
  * }) // throws error
+ *
+ * @see {@link settleObject} if you don't want to fail if some promises reject
  */
-export const resolveObject = async <T extends Record<string, Promise<any>>>(object: T): Promise<Resolved<T>> => {
-  // Create promises for each entry 
+export const resolveObject = async <T extends Record<PropertyKey, Promise<any>>>(object: T): Promise<Resolved<T>> => {
+  // Create promises for each entry
   const entryPromises = Object.entries(object)
     .map(([key, promise]) => promise.then(value => [key, value]))
-  
+
   // Resolve the promises
   const resolvedEntries = await Promise.all(entryPromises)
-  
+
   // Reconstruct object
   return Object.fromEntries(resolvedEntries)
 }
